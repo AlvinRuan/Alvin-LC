01e1643181db2fae4e3a91eba30cfa99e0c51115
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-07-25 00:05:55 -0400

    Time: 45 ms (70.25%), Space: 16.4 MB (55.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -9,21 +9,16 @@ class Solution:
         # iterate through each variable in the string, we want to see if its a left side of key-value pair or right side - if its left side, add to the end of the stack
         
         
+        stack = []
+
         for i in s:
-            if i in '([{':
+            if i not in '])}':
                 stack.append(i)
-            
-            # if its right side, check the last addition on the stack, and see if it matches the corresponding other side, if not return false, if so, pop the last addition
             elif len(stack) > 0:
                 popped = stack.pop()
-            
                 if (popped == '(' and i != ')') or (popped == '[' and i != ']') or (popped == '{' and i != '}'):
                     return False
-            # code block to execute if any of the conditions are True
             else:
                 return False
-
-            
-        if len(stack) == 0:
-            return True
+        return False if len(stack) > 0 else True
     
