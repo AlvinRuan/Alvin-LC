55d6511e9b72d327a57d22af16f217f5a93e4a0d
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-07-30 23:39:50 -0400

    Time: 92 ms (99.51%), Space: 21 MB (90.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -23,10 +23,9 @@ class Solution:
         for i in collection:
         # reverse sort array
             result.append([collection[i], i])
-        print(result)  
         # largest_to_smallest = sorted(result,key=,reverse=True)
         largest_to_smallest = sorted(result, reverse=True)
-        print("after sort", largest_to_smallest) 
+
         # another for loop decrementing the value of K
         
         # appending each one to the result array
