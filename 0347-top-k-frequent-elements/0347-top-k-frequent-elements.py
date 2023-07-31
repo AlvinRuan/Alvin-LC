387cb5cd0ad98d24ae72ea37906a899f9fac7414
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-07-30 23:39:34 -0400

    Time: 106 ms (92.50%), Space: 21.2 MB (41.12%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,39 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # initial a collection / hashmap
+        
+        collection = {}
+        
+        # loop through the array of numbers
+        
+        for i in nums:
+        # if number, doesn't exist, hashmap[number] = 1
+        
+            if i in collection:
+                collection[i] += 1
+            else:
+                collection[i] = 1
+        # else hashmap[number] += 1
+        
+        # loop through hashmap, and append numbers to empty array = result
+        
+        result = []
+        
+        for i in collection:
+        # reverse sort array
+            result.append([collection[i], i])
+        print(result)  
+        # largest_to_smallest = sorted(result,key=,reverse=True)
+        largest_to_smallest = sorted(result, reverse=True)
+        print("after sort", largest_to_smallest) 
+        # another for loop decrementing the value of K
+        
+        # appending each one to the result array
+        
+        resulting = []
+        
+        for i in range(k):
+            resulting.append(largest_to_smallest[i][1])
+            
+        return resulting
