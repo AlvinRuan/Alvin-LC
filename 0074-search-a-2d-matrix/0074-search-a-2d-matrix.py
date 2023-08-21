25b853b0ad6145d51c6fd891beb434231c945834
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-08-20 23:16:56 -0400

    Time: 50 ms (67.52%), Space: 17 MB (21.20%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,30 +1,41 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
               
-        ROWS, COLS = len(matrix), len(matrix[0])
-
-        top, bot = 0, ROWS - 1
-        while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bot = row - 1
-            else:
-                break
-
-        if not (top <= bot):
+        if len(matrix) == None:
             return False
-        row = (top + bot) // 2
-        l, r = 0, COLS - 1
-        while l <= r:
-            m = (l + r) // 2
-            if target > matrix[row][m]:
-                l = m + 1
-            elif target < matrix[row][m]:
-                r = m - 1
+        
+        
+        target_matrix = None
+        
+        if len(matrix) == 1:
+            target_matrix = matrix[0]
+        
+        top, bottom = 0, len(matrix)-1
+        
+        while top <= bottom:
+            mid = (top+bottom)//2
+            if target > matrix[mid][-1]:
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bottom = mid - 1
+            else:
+                target_matrix = matrix[mid]
+                break
+                
+        if target_matrix == None:
+            return False
+        
+        left, right = 0, len(target_matrix)-1
+        
+        while left <= right:
+            mid = (right+left)//2
+            if target_matrix[mid] > target:
+                right = mid - 1
+            elif target_matrix[mid] < target:
+                left = mid + 1
             else:
                 return True
+            
         return False
             
         
