b6994c00ceb465563c9d31e7f74ac45e8938da52
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-08-21 22:41:36 -0400

    Time: 205 ms (13.89%), Space: 16.6 MB (7.31%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        collection = {}
+        
+        left, right, longest = 0,0,0
+        
+        # parse through the string, we add to the set, as long as the amount is not greater than k's value
+
+        while right < len(s):
+            
+            if s[right] in collection:
+                collection[s[right]] += 1
+            else:
+                collection[s[right]] = 1
+                
+            most_common = max(collection, key=collection.get)
+            check = right - left + 1 - collection[most_common]
+            while check > k:
+                collection[s[left]] -= 1
+                left += 1
+                most_common = max(collection, key=collection.get)
+                check = right - left + 1 - collection[most_common]
+            longest = max(longest, right-left+1)
+            right += 1
+        return longest
