f610d25be638bc7aa91e2aded47ee1afcb8cb647
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-08-30 22:56:39 -0400

    Time: 287 ms (5.02%), Space: 23.7 MB (81.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
         result = []
         
-        # I want to get the prefix product into the corresponding result location
         prefix = 1
         for i in range(len(nums)):
             result.append(prefix)
             prefix *= nums[i]
-        # do the same with suffix and multiply against the corresponding result location value
+            
+        print(result)
         postfix = 1
-        for i in range(len(nums)-1,-1,-1):
-            result[i] *= postfix
+        for i in range(len(nums)-1, -1, -1):
+            print("i==>", result[i])
+            result[i] = postfix * result[i]
+            print("new result[i]",result[i])
             postfix *= nums[i]
-        
-        
-        
+
+            # [1,2,3,4] 1
+            # [1,1,2,6]
+            # [1,1,2,6]
+            #   12 4 1
+            # [x,8,8,6]
+        print(result)
         return result
+            
