33b3aa721b39d6c2899807c1b57408835c5dbd5c
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-09-16 18:07:06 -0400

    Time: 640 ms (98.56%), Space: 36.5 MB (24.00%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -4,8 +4,5 @@ class Solution:
         arr = [pref[0]]
         
         for i in range(1,len(pref)):
-            print(i)
             arr.append(pref[i] ^ pref[i-1])
-        # arr[0] ^ pref[0] = x
-        # 2 = pref
         return arr
