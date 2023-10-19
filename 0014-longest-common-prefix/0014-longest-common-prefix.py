c826c21df800d35c4af72d4e0bfb969b55227277
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2023-10-18 22:52:44 -0400

    Time: 37 ms (83.03%), Space: 16.4 MB (47.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,7 +2,7 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
         
-        shortest = min(strs, key=len)
+        shortest = min(strs)
         
         for i in strs:
             while shortest != "":
