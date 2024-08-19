5e2fc1ecd0da3ca07b9510a4a1a1d38bc03b22ef
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2024-08-18 21:24:49 -0400

    Time: 87 ms (66.76%), Space: 21 MB (89.6%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
 
         collection = {}
-        
+
         for i in nums:
             if i in collection:
                 collection[i] += 1
             else:
                 collection[i] = 1
-  
-        sorted_collection = sorted(collection, key=lambda i:-collection[i])
-
         
-        return sorted_collection[:k]
+        sort_collection = sorted(collection, key=lambda x:-collection[x])
+        return sort_collection[0:k]
+        
