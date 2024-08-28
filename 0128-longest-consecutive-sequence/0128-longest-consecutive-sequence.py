0c649f2d28de477e342845d102454aa66969424f
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2024-08-27 21:46:36 -0400

    Time: 4318 ms (19.36%), Space: 31.8 MB (31.52%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -7,10 +7,9 @@ class Solution:
 
         for i in nums:
             if i-1 not in set_version:
-                current_longest = 1
-                while i+1 in set_version:
+                current_longest = 0
+                while i+current_longest in set_version:
                     current_longest += 1
-                    i+= 1
                 longest = max(current_longest, longest)
         
         return longest
