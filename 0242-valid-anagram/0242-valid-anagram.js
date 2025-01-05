4aa18490f3c08cee221fa14e76512e93541cd189
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-05 14:17:37 -0500

    Time: 35 ms (6.45%), Space: 58.5 MB (5.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,18 +7,20 @@ var isAnagram = function(s, t) {
     let collection = {}
 
     for (var i in s) {
-        if (s[i] in collection) {
-            collection[s[i]] += 1
+        let char = s[i]
+        if (char in collection) {
+            collection[char] += 1
         } else {
-            collection[s[i]] = 1
+            collection[char] = 1
         }
     }
     
     for (var j in t) {
-        if (!t[j] in collection) {
+        let char = t[j]
+        if (!char in collection) {
             return false
         } else {
-                collection[t[j]] -= 1
+                collection[char] -= 1
         }
     }
 
