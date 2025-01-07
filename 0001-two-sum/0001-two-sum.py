02d5bddfd38b708bcb51cdd2b27dc60cb3adedcb
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-06 22:43:10 -0500

    Time: 0 ms (100%), Space: 18.8 MB (18.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,11 +4,11 @@ class Solution:
         collection = {}
 
         for i in range(len(nums)):
-
-            opposite = target - nums[i]
-
-            if opposite in collection:
-                return [i, collection[opposite]]
-
+            other_half = target - nums[i] #  other_half(7) = 9 - i(2)
+                                          #  other_half(2) = 9 - i(7)
+            # if we find 7, we want to pair that index with index of i
+            # else, we add the pair and index into the collection
+            if other_half in collection:
+                return [i, collection[other_half]]
             else:
-                collection[nums[i]] = i
+                collection[nums[i]] = i # {2 : 0}
