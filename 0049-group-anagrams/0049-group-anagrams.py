759a0eec3b45b4d1c52425e0fe1bb8b5ead7e82c
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-07 23:05:43 -0500

    Time: 19 ms (38.5%), Space: 20.7 MB (48.8%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -8,27 +8,35 @@ class Solution:
 
         # if strs.length === 1:
         # return strs
+        # start a dictionary to collect as we loop
+        # as we loop through this
+        # rearrange the existing increment into alphanumeric order
+        # check if it exists in the dictionary
+        # if it does, we push onto the array, the un-alphanumeric ordered string
+        # if it does not, we create the key value pair with they key being the alpa-numeric'd string
+        # and the value being [ the incremented item ]
+
         if len(strs) == 1:
             return [strs]
 
-        # start a dictionary to collect as we loop
+        
         collection = {}
-        # as we loop through this
+        
         for i in strs:
-        # rearrange the existing increment into alphanumeric order
+        
             print(i)
             alpha_order = ''.join(sorted(i))
             print(alpha_order)
-        # check if it exists in the dictionary
+        
             if alpha_order in collection:
-        # if it does, we push onto the array, the un-alphanumeric ordered string
+        
                 collection[alpha_order].append(i)
-        # if it does not, we create the key value pair with they key being the alpa-numeric'd string
+        
             else:
                 collection[alpha_order] = [i]
-            # and the value being [ the incremented item ]
+            
         results = []
-        
+            
         for j in collection:
             results.append(collection[j])
         
