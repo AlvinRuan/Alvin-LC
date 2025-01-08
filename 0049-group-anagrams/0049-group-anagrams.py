1cd1ce7bc6c58dfac404b70ca1ef76a8737521be
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-07 23:06:00 -0500

    Time: 16 ms (43.99%), Space: 20.6 MB (60.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,21 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        # Input = an array of strings
-        # Output = an array of arrays
-        # Contraints
-        # 
-
-        # if strs.length === 1:
-        # return strs
-        # start a dictionary to collect as we loop
-        # as we loop through this
-        # rearrange the existing increment into alphanumeric order
-        # check if it exists in the dictionary
-        # if it does, we push onto the array, the un-alphanumeric ordered string
-        # if it does not, we create the key value pair with they key being the alpa-numeric'd string
-        # and the value being [ the incremented item ]
-
         if len(strs) == 1:
             return [strs]
 
