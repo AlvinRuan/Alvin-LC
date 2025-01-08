3747078de5ce2fb899daca0456bb3f332994d434
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-07 23:04:41 -0500

    Time: 18 ms (40.51%), Space: 20.7 MB (54.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,39 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        collection = {
+        # Input = an array of strings
+        # Output = an array of arrays
+        # Contraints
+        # 
 
-        }
+        # if strs.length === 1:
+        # return strs
+        if len(strs) == 1:
+            return [strs]
 
+        # start a dictionary to collect as we loop
+        collection = {}
+        # as we loop through this
         for i in strs:
-            alphabetized = ''.join(sorted(i))
-            if alphabetized in collection:
-                collection[alphabetized].append(i)
-                print(collection[alphabetized])
+        # rearrange the existing increment into alphanumeric order
+            print(i)
+            alpha_order = ''.join(sorted(i))
+            print(alpha_order)
+        # check if it exists in the dictionary
+            if alpha_order in collection:
+        # if it does, we push onto the array, the un-alphanumeric ordered string
+                collection[alpha_order].append(i)
+        # if it does not, we create the key value pair with they key being the alpa-numeric'd string
             else:
-                collection[alphabetized] = [i]
+                collection[alpha_order] = [i]
+            # and the value being [ the incremented item ]
+        results = []
+        
+        for j in collection:
+            results.append(collection[j])
+        
+        return results
+        
 
-        result = []
-
-        for i in collection:
-            result.append(collection[i])
-
-        return result
-            
+        
+        
