b8dc98757f5fd6a44556a45113c6c22af73d26ac
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-10 23:11:14 -0500

    Time: 17 ms (91.89%), Space: 23.4 MB (46.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        result = []
+        # Input = an array
+        # Output = array of products
+        # Contraints = cannot use division & must be O(n)
+        # Edge Case = when there's 0s, the result is zero
 
-        prefix = 1
-        for i in nums:
-            result.append(prefix)
-            prefix *= i
+        prefix = []
 
-        postfix = 1
+        # as we loop through the nums,
+        # [1, 2, 3, 4]
+        # [1, 1, 2, 6]
+        prefix_accum = 1
+        for i in range(len(nums)):
+            prefix.append(prefix_accum)
+            prefix_accum *= nums[i]
+        
 
-        for j in range(len(nums)-1, -1, -1):
-            result[j] *= postfix
-            postfix *= nums[j]
+        postfix_accum = 1
+        # [1, 2, 3, 4]
+        # [1, 1, 2, 6]
+        # [24, 12, 8, 6]
+        for j in range(len(nums)-1,-1,-1):
+            prefix[j] *= postfix_accum
+            postfix_accum *= nums[j]
             
-
-        return result
+        return prefix
