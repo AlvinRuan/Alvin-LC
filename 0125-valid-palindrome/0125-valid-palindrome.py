e5c78b108ed28d5f0e5a43c4a2eddd6cd5aea390
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-11 23:54:06 -0500

    Time: 12 ms (19.45%), Space: 18.2 MB (39.8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        # add all a-z characters into a string
+        
         words = ""
         for i in s:
             if i.isalnum():
                 words += i.lower()
-        # left and right pointer
+        
         left = 0
         right = len(words)-1
-        # while right > left
+        
         while left < right:
-        # check left & right pointer
+        
             if words[left] == words[right]:
                 left += 1
                 right -= 1
             else:
                 return False
-        # if equal, increment left, decrement right, continue loop
-        # else return false
-        # return false if successfully finished while loop
+        
         return True
