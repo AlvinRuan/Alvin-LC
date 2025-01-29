e108cb24325a5f96e53f0d28c7c4a6b83ffaa95b
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-01-29 11:55:27 -0500

    Time: 0 ms (100%), Space: 18.1 MB (7.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,29 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        stack = []
-        
-        # Input = string of arrays
-        # Output = T/F
-        
-        # iterate through each variable in the string, we want to see if its a left side of key-value pair or right side - if its left side, add to the end of the stack
-        
-        
+
         stack = []
 
+        pairs = {
+            "}" : "{",
+            ")" : "(" ,
+            "]" : "[" 
+        }
+
         for i in s:
-            if i not in '])}':
-                stack.append(i)
-            elif len(stack) > 0:
-                popped = stack.pop()
-                if (popped == '(' and i != ')') or (popped == '[' and i != ']') or (popped == '{' and i != '}'):
+            if i in pairs:
+                if len(stack) > 0:
+                    popped = stack.pop()
+                    if pairs[i] != popped:
+                        return False
+                else:
                     return False
             else:
-                return False
-        return False if len(stack) > 0 else True
-    
+                stack.append(i)
+        
+        if len(stack) != 0:
+            return False
+        else:
+            return True
+                
+            
