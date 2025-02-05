aaae9c5150ddb73605f5f45b5298242b114cc8c3
Alvin Ruan <113706094+AlvinRuan@users.noreply.github.com>
2025-02-04 22:24:41 -0500

    Time: 600 ms (45.69%), Space: 20.6 MB (61.5%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,29 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        
-        # input is a list
-        # output is an array of distinct triplets that equal 0
-        # exceptions: when given nums does not add up to 0 => return empty array
-        
-        collection = []
+        # Input = Array
+        # Output = array of arrays that contain the sum of elements which = 0
+        # Constraints = i !=j, i !=k, j != k 
+
+        result = []
 
         nums.sort()
         
-        for index,i in enumerate(nums):
-            if index > 0 and i == nums[index-1]:
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i -1]:
                 continue
-            left, right = index+1, len(nums)-1
+            left = i + 1 
+            right = len(nums)-1
             while left < right:
-                threeSum = i + nums[left] + nums[right]
-                if threeSum > 0:
-                    right -= 1
-                elif threeSum < 0:
-                    left +=1
-                else:
-                    collection.append([i, nums[left], nums[right]])
+                current_sum = nums[i] + nums[left] + nums[right]
+                if current_sum == 0:
+                    result.append([nums[i], nums[left] ,nums[right]])
                     left += 1
-                    while nums[left] == nums[left-1] and left < right:
+                    while nums[left] == nums[left - 1 ] and left < right:
                         left += 1
+                elif current_sum > 0:
+                    right -= 1
+                else:
+                    left += 1
         
-        return collection
+        return result
+
+
